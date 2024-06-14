271be23f518afb46b372b1336ec1011bf643ad27
Hasan Yahya <hasna786yahya@gmail.com>
2024-06-14 22:04:30 +0500

    Refactor Playground component UI and optimize ScrollArea height

--- src/components/component/playground.jsx
+++ src/components/component/playground.jsx
@@ -231,7 +231,7 @@ export function Playground() {
                 <main className="flex-1">
                     <div className="flex h-full">
                         <div className="w-[39%] border-r">
-                            <Tabs className="h-auto" defaultValue="all">
+                            <Tabs defaultValue="all">
                                 <div className="justify-center">
                                     <div className="flex p-2 font-bold text-xl h-16 bg-white border-r border-b border-l self-center">
                                         <span className="ml-2 p-2">
@@ -266,46 +266,49 @@ export function Playground() {
                                     </div>
                                 </div>
 
-                                <ScrollArea className="h-[calc(100%-30px)] border mt-4 mb-auto" rows={1}
-                                    style={{ height: 'calc(100% - 30px)' }}
-                                >
+                                <TabsContent value="all">
 
-                                    {/*<div key={index} className="flex p-4 space-x-4 border-b hover:bg-gray-50">*/}
+                                    <ScrollArea className="h-[calc(100%-30px)] border mt-4 mb-auto" rows={1}
+                                        style={{ height: 'calc(100% - 30px)' }}
+                                    >
 
-                                    <div className="h-[calc(100%-64px)] object-contain">
-                                        {Array(10).fill().map((_, index) => (
-                                            <div className="flex p-4 space-x-4 border-b hover:bg-gray-50">
-                                                <Avatar>
-                                                    <AvatarImage src="/placeholder-user.jpg" />
-                                                    <AvatarFallback className="bg-white border border-gray-200 text-gray-500"
-                                                    >U</AvatarFallback>
-                                                </Avatar>
-                                                <div className="flex flex-col flex-1 min-w-10">
-                                                    <div className="flex">
-                                                        <span className="font-medium truncate">Name Surname</span>
-                                                        <div className="flex ml-auto mb-1">
-                                                            <TooltipProvider>
-                                                                <Tooltip>
-                                                                    <TooltipTrigger asChild>
-                                                                        <span className="text-sm text-gray-500 whitespace-nowrap self-end">8 months ago</span>
-                                                                    </TooltipTrigger>
-                                                                    <TooltipContent className="text-sm font-normal text-gray-500">Sent on 2023-01-01 at 8:00 PM</TooltipContent>
-                                                                </Tooltip>
-                                                            </TooltipProvider>
+                                        {/*<div key={index} className="flex p-4 space-x-4 border-b hover:bg-gray-50">*/}
+
+                                        <div className="h-[calc(100%-64px)] object-contain">
+                                            {Array(10).fill().map((_, index) => (
+                                                <div className="flex p-4 space-x-4 border-b hover:bg-gray-50">
+                                                    <Avatar>
+                                                        <AvatarImage src="/placeholder-user.jpg" />
+                                                        <AvatarFallback className="bg-white border border-gray-200 text-gray-500"
+                                                        >U</AvatarFallback>
+                                                    </Avatar>
+                                                    <div className="flex flex-col flex-1 min-w-10">
+                                                        <div className="flex">
+                                                            <span className="font-medium truncate">Name Surname</span>
+                                                            <div className="flex ml-auto mb-1">
+                                                                <TooltipProvider>
+                                                                    <Tooltip>
+                                                                        <TooltipTrigger asChild>
+                                                                            <span className="text-sm text-gray-500 whitespace-nowrap self-end">8 months ago</span>
+                                                                        </TooltipTrigger>
+                                                                        <TooltipContent className="text-sm font-normal text-gray-500">Sent on 2023-01-01 at 8:00 PM</TooltipContent>
+                                                                    </Tooltip>
+                                                                </TooltipProvider>
+                                                            </div>
                                                         </div>
+                                                        <span className="text-sm text-gray-500 truncate">Subject of the email</span>
+                                                        <span className="text-sm text-gray-500 truncate">Preview of the email content...</span>
                                                     </div>
-                                                    <span className="text-sm text-gray-500 truncate">Subject of the email</span>
-                                                    <span className="text-sm text-gray-500 truncate">Preview of the email content...</span>
+
+                                                    {/*</div>*/}
                                                 </div>
-
-                                                {/*</div>*/}
-                                            </div>
-                                        ))}
-                                    </div>
+                                            ))}
+                                        </div>
 
 
-                                </ScrollArea>
+                                    </ScrollArea>
 
+                                </TabsContent>
 
                             </Tabs>
                         </div>
